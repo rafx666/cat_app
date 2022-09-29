d98dc0f55425873d23bd5c51c7111a60ff8b723b
Rafał Pieczyk <48286671+rafx666@users.noreply.github.com>
2022-09-29 15:33:36 +0200

    appbar implementation

--- cat_app/lib/main.dart
+++ cat_app/lib/main.dart
@@ -9,12 +9,8 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      home: const FirstPage(),
+    return const MaterialApp(
+      home: FirstPage(),
     );
   }
 }
@@ -39,7 +35,7 @@ class FirstPage extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
               const SizedBox(
-                height: 45,
+                height: 50,
               ),
               Text(
                 'CatApp',
@@ -135,6 +131,10 @@ class SecondPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: const PreferredSize(
+        preferredSize: Size.fromHeight(45),
+        child: CatBar(),
+      ),
       body: Container(
         decoration: const BoxDecoration(
           image: DecorationImage(
@@ -145,85 +145,27 @@ class SecondPage extends StatelessWidget {
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              const SizedBox(
-                height: 45,
-              ),
-              Text(
-                'CatApp',
-                style: GoogleFonts.poppins(
-                  fontSize: 50,
-                  color: Colors.white,
-                ),
-              ),
-              const SizedBox(
-                height: 50,
-              ),
-              const SizedBox(
-                height: 480,
-              ),
-              Container(
-                  height: 60,
-                  width: 250,
-                  decoration: const BoxDecoration(
-                    border: Border(
-                      top: BorderSide(
-                          color: Colors.black,
-                          width: 3,
-                          style: BorderStyle.solid),
-                      bottom: BorderSide(
-                          color: Colors.black,
-                          width: 3,
-                          style: BorderStyle.solid),
-                      left: BorderSide(
-                          color: Colors.black,
-                          width: 3,
-                          style: BorderStyle.solid),
-                      right: BorderSide(
-                          color: Colors.black,
-                          width: 3,
-                          style: BorderStyle.solid),
-                    ),
-                    color: Colors.white,
-                  ),
-                  alignment: Alignment.center,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.white,
-                      minimumSize: const Size(
-                        250,
-                        60,
-                      ),
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Icon(
-                          Icons.arrow_back,
-                          color: Colors.black,
-                          size: 40,
-                        ),
-                        const SizedBox(
-                          width: 40,
-                        ),
-                        Text(
-                          'WRÓĆ',
-                          style: GoogleFonts.amaticSc(
-                            fontSize: 38,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black,
-                          ),
-                        ),
-                      ],
-                    ),
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                  )),
-            ],
+            children: const [],
           ),
         ),
       ),
+    );
+  }
+}
+
+class CatBar extends StatelessWidget {
+  const CatBar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text(
+        'CatApp demo',
+        style: GoogleFonts.poppins(),
+      ),
+      backgroundColor: Colors.brown,
     );
   }
 }
