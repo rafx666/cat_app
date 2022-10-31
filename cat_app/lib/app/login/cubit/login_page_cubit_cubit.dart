89cac04268900025d7862b3d5d6a35b29055e3eb
Rafał Pieczyk <48286671+rafx666@users.noreply.github.com>
2022-10-31 11:04:33 +0100

    Upgrade LoginPageCubit

--- cat_app/lib/app/login/cubit/login_page_cubit_cubit.dart
+++ cat_app/lib/app/login/cubit/login_page_cubit_cubit.dart
@@ -7,25 +7,55 @@ import 'package:meta/meta.dart';
 part 'login_page_cubit_state.dart';
 
 class LoginPageCubit extends Cubit<LoginPageState> {
-  LoginPageCubit() : super(LoginPageState());
+  LoginPageCubit()
+      : super(const LoginPageState(errorMessage: '', isLogged: false));
+
+  Future<void> start() async {
+    emit(const LoginPageState(
+      errorMessage: '',
+      isLogged: false,
+    ));
+  }
 
   Future<void> login({
     required email,
     required password,
   }) async {
-    await FirebaseAuth.instance.signInWithEmailAndPassword(
-      email: email,
-      password: password,
-    );
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      emit(const LoginPageState(
+        errorMessage: '',
+        isLogged: true,
+      ));
+    } catch (error) {
+      emit(const LoginPageState(
+        errorMessage: 'Błąd logowania',
+        isLogged: false,
+      ));
+    }
   }
 
   Future<void> signIn({
     required email,
     required password,
   }) async {
-    await FirebaseAuth.instance.createUserWithEmailAndPassword(
-      email: email,
-      password: password,
-    );
+    try {
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      emit(const LoginPageState(
+        errorMessage: '',
+        isLogged: true,
+      ));
+    } catch (error) {
+      emit(const LoginPageState(
+        errorMessage: 'Błąd rejestracji',
+        isLogged: false,
+      ));
+    }
   }
 }
--- cat_app/lib/app/login/cubit/login_page_cubit_state.dart
+++ cat_app/lib/app/login/cubit/login_page_cubit_state.dart
@@ -1,4 +1,11 @@
+// ignore_for_file: prefer_typing_uninitialized_variables
+
 part of 'login_page_cubit_cubit.dart';
 
 @immutable
-class LoginPageState {}
+class LoginPageState {
+  final errorMessage;
+  final bool isLogged;
+
+  const LoginPageState({required this.errorMessage, required this.isLogged});
+}
